a2c11a1bc0701ea9eab6fb9e54958e7214512262
sebbex1337 <sefort@hotmail.dk>
2023-08-24 11:06:34 +0200

    Made backend for frontend dat-js-intro-crud

--- app.js
+++ app.js
@@ -1,8 +1,69 @@
 import express from "express";
+import fs from "fs/promises";
+import cors from "cors";
 
 const app = express();
 const port = 3333;
 
+app.use(express.json());
+app.use(cors());
+
 app.listen(port, () => {
-	console.log(`App is running on http://localhost${port}`);
+	console.log(`App is running on http://localhost:${port}`);
+});
+
+app.get("/", (request, response) => {
+	response.send("Hello world!");
+});
+
+app.get("/test", (request, response) => {
+	response.send("This is a test");
+});
+
+app.get("/users", async (request, response) => {
+	const data = await fs.readFile("data.json");
+	const users = JSON.parse(data);
+	console.log(users);
+	response.json(users);
+});
+
+app.post("/users", async (request, response) => {
+	const newUser = request.body;
+	newUser.id = new Date().getTime();
+	console.log(newUser);
+
+	const data = await fs.readFile("data.json");
+	const users = JSON.parse(data);
+
+	users.push(newUser);
+	fs.writeFile("data.json", JSON.stringify(users));
+	response.json(users);
+});
+
+app.put("/users/:id", async (request, response) => {
+	const id = Number(request.params.id);
+	console.log(id);
+
+	const data = await fs.readFile("data.json");
+	const users = JSON.parse(data);
+	let userToUpdate = users.find((user) => user.id === id);
+	const body = request.body;
+	userToUpdate.image = body.image;
+	userToUpdate.mail = body.mail;
+	userToUpdate.name = body.name;
+	userToUpdate.title = body.title;
+	fs.writeFile("data.json", JSON.stringify(users));
+	response.json(users);
+});
+
+app.delete("/users/:id", async (request, response) => {
+	const id = Number(request.params.id);
+	console.log(id);
+	const data = await fs.readFile("data.json");
+	const users = JSON.parse(data);
+
+	const newUsers = users.filter((user) => user.id !== id);
+	fs.writeFile("data.json", JSON.stringify(newUsers));
+
+	response.json(users);
 });
